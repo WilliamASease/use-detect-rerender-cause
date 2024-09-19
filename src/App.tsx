bf4d23091af106c33c91876435e159bbda037c7c
William A. Sease <WilliamASease@gmail.com>
2024-09-19 01:50:26 -0400

    pretty

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,27 @@ function App() {
           width: "66%",
         }}
       >
+        <div
+          style={{
+            margin: 5,
+            padding: 5,
+            border: "solid black 1px",
+            background: "white",
+          }}
+        >
+          <span style={{ fontWeight: "bold" }}>use-detect-rerender-cause</span>
+          <span style={{ marginLeft: 5 }}>| WilliamASease 2024 |</span>
+          <a
+            style={{ marginLeft: 5 }}
+            href="https://github.com/WilliamASease/use-detect-rerender-cause"
+          >
+            repository
+          </a>
+          <span> |</span>
+          <a style={{ marginLeft: 5 }} href="https://williamasease.github.io">
+            homepage
+          </a>
+        </div>
         <Cases />
       </div>
     </div>
